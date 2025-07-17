be2a84267752476dd518d0f744cc0304316426e9
Nguyen Tan Khoa <tankhoa1334@gmail.com>
2025-07-17 13:51:08 +0700

    update product card

--- components/shared/ProductCard.tsx
+++ components/shared/ProductCard.tsx
@@ -15,61 +15,58 @@ interface ProductCardProps {
 }
 
 export const ProductCard = ({ product, isFeatured }: ProductCardProps) => {
-  const Price = () => {
-    if (product.discount > 0) {
-      const price = product.price - product.discount;
-      const percentage = (product.discount / product.price) * 100;
-      return (
-        <div className="flex gap-2 items-center">
-          <Badge className="flex gap-4" variant="destructive">
-            <div className="line-through">${product.price}</div>
-            <div>%{percentage.toFixed(2)}</div>
-          </Badge>
-          <h2>${price.toFixed(2)}</h2>
-        </div>
-      );
-    }
-
-    return <h2>${product.price}</h2>;
-  };
-
   return (
     <Link href={`/products/${product.slug}`}>
       <Card className="h-full pt-0 pb-2 gap-0">
-        <CardHeader className="p-0">
-          <div className="relative h-50 w-full">
-            <Image
-              className="rounded-t-lg"
-              src={product.images[0]}
-              alt="product image"
-              fill
-              sizes="(min-width: 1000px) 30vw, 50vw"
-              style={{ objectFit: "cover" }}
-            />
-          </div>
+        <CardHeader className="p-0 relative h-50 w-full">
+          <Image
+            className="rounded-t-lg object-contain"
+            src={product.images[0]}
+            alt={product.name}
+            fill
+          />
+          {product.discount > 0 && (
+            <div className="absolute bottom-2 left-4 flex flex-wrap gap-1 z-10">
+              <Badge
+                variant="destructive"
+                className="text-xs px-1  shadow-sm rounded-full"
+              >
+                -{product.discount}%
+              </Badge>
+            </div>
+          )}
         </CardHeader>
-        <CardContent className="grid gap-1 px-4 py-2">
+        <CardContent className="px-4">
           {isFeatured &&
             product.categoryNames.map((categoryName) => (
-              <Badge
-                key={categoryName}
-                variant="outline"
-                className="w-min text-neutral-500 mr-1"
-              >
-                {categoryName}
-              </Badge>
+              <div key={categoryName} className="pt-2">
+                <Badge variant="outline" className="text-neutral-500 mr-1">
+                  {categoryName}
+                </Badge>
+              </div>
             ))}
-          <h2>{product.name}</h2>
-          <p className="text-xs text-neutral-500 text-justify">
-            {product.description}
-          </p>
+          <div className="flex flex-col items-start gap-1 pt-2">
+            <h2 className="text-md font-semibold">{product.name}</h2>
+            <p className="text-xs text-neutral-500 line-clamp-2 wrap-anywhere">
+              {product.description}
+            </p>
+          </div>
         </CardContent>
-        <CardFooter>
-          {/* {product.status === "AVAILABLE" ? (
-            <Price />
-          ) : (
-            <Badge variant="secondary">Out of stock</Badge>
-          )} */}
+        <CardFooter className="mt-auto px-4">
+          <div className="flex items-start py-1 gap-2">
+            <div>
+              <span className="text-md font-semibold">
+                ${product.price.toFixed(2)}
+              </span>
+            </div>
+            {product.discount > 0 && (
+              <div>
+                <span className="text-xs font-normal text-neutral-400 line-through">
+                  ${(product.price * (1 - product.discount / 100)).toFixed(2)}
+                </span>
+              </div>
+            )}
+          </div>
         </CardFooter>
       </Card>
     </Link>
